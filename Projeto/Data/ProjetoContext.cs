4d6bb198027b2c88fff6e2b2d81dd23ea2089518
Gustavo Lopes de Paula <920005@ptin.corpPT.com>
2020-03-26 12:34:19 -0300

    Scaffold

---
+++ Projeto/Data/ProjetoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Models;
+
+namespace Projeto.Data
+{
+    public class ProjetoContext : DbContext
+    {
+        public ProjetoContext (DbContextOptions<ProjetoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto.Models.Department> Department { get; set; }
+    }
+}
--- Projeto/Startup.cs
+++ Projeto/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Data;
 
 namespace Projeto
 {
@@ -33,6 +35,9 @@ namespace Projeto
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
